1da28acf945d4e8fc6055fe0af0e90f4522278cc
DongshiXu <623752785@qq.com>
2020-05-30 13:56:22 +0800

    update

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -163,9 +163,9 @@
             <span :class="['toolbar-top-cluter-span2']" :style={}># 采样:</span>
           </p>
           <p :class="['toolbar-top-cluter-select']">
-            <el-select v-model="cluter" placeholder="请选择" size=mini>
+            <el-select v-model="cluster" placeholder="请选择" size=mini>
               <el-option
-                v-for="item in cluterOptions"
+                v-for="item in clusterOptions"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value">
@@ -228,13 +228,14 @@ export default {
       testData: {'0': [1261, 480, 348, 1518, 789, 49, 20, 1557, 596, 1107], '1': [0, 0, 1, 0, 0, 0, 0, 0, 0, 0], '2': [1384, 1849, 280, 3145, 3128, 269, 73, 2900, 2220, 1891], '3': [0, 0, 0, 0, 0, 0, 0, 1, 0, 0], '4': [459, 132, 1332, 513, 274, 101, 48, 384, 215, 345], '5': [488, 2112, 165, 1489, 2021, 362, 27, 1749, 629, 561], '6': [52, 466, 42, 227, 287, 403, 5, 138, 92, 51], '7': [1305, 1157, 198, 1952, 1496, 115, 89, 2149, 759, 827], '8': [491, 450, 140, 1820, 1272, 111, 32, 1018, 1853, 851], '9': [74, 109, 35, 104, 94, 18, 47, 189, 56, 32]},
       testRawData: '',
       sample: 1,
-      cluter: 0,
+      cluster: 1,
       timeValue: '2017-05-01',
       colorValue: 'color1',
-      cluterOptions: [
+      dataContainer: '',
+      clusterOptions: [
         {
-          value: 0,
-          lable: 0
+          value: 1,
+          lable: 1
         },
         {
           value: 5,
@@ -286,7 +287,8 @@ export default {
   },
   mounted () {
     this.initMap()
-    this.getData('2017-05-01', 10)
+    // this.getData('2017-05-01', 10)
+    this.getDataTest()
     this.drawODMatrix(this.testData)
     // console.log(d3)
   },
@@ -295,7 +297,7 @@ export default {
       var newmap = L.tileLayer('http://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetGray/MapServer/tile/{z}/{y}/{x}')
       let initMap = L.map('map', {
         center: [20.016097083891825, 110.33584131749194],
-        zoom: 13,
+        zoom: 12,
         layers: [newmap],
         zoomControl: false
       })
@@ -306,44 +308,57 @@ export default {
     original () {
       d3.selectAll('.circle').remove()
       d3.selectAll('.road').remove()
-      console.log(typeof (this.sample))
-      if (this.sample === 1 && this.cluter === 0) {
-        this.ODNum = this.testRawData[0]['o'].length
-        this.testRawData[0]['o'].forEach(latlng => {
-          this.drawSignalCircle(latlng, 'circle', 'red', 1)
-        })
+      d3.select('.lineChart').remove()
+      let sampleTemp = this.sample.toString()
+      let clusterTemp = this.cluster.toString()
+      let colorTemp = this.colorValue
+      if (colorTemp === 10) {
+        this.color = d3.schemeCategory10
       } else {
-        for (let k in this.testRawData[1]) {
-          this.ODNum += this.testRawData[1][k].length
-        }
-        // 起点聚类效果
-        Object.keys(this.testRawData[1]).forEach((res, i) => {
-          if (this.testRawData[1][res].length !== 0) {
-            this.drawCircle(this.testRawData[1][res], this.color[i])
-            // this.drawLine(this.testRawData[1][res], this.color[i], i)
-          }
-          // lineChartData.push([i, this.testRawData[1][res].length])
-        })
-        // this.drawLineChart(lineChartData)
+        this.color = d3.schemeTableau10
       }
+      let currentData = this.dataContainer['cluster'][clusterTemp]['sample'][sampleTemp]
+      this.ODNum = currentData[sampleTemp].length
+      currentData[sampleTemp].forEach((val, i) => {
+        let cn = 'circle' + val[2][0]
+        this.drawSignalCircle(val[0], 'circle ' + cn, this.color[val[2][0]], 1)
+      })
+      let lineChartData = this.dataContainer['cluster'][clusterTemp]['statistic'][sampleTemp][0]['o']
+      this.drawLineChart(lineChartData)
     },
     destination () {
       d3.selectAll('.circle').remove()
       d3.selectAll('.road').remove()
-      if (this.sample === 1 && this.cluter === 0) {
-        this.ODNum = this.testRawData[0]['d'].length
-        this.testRawData[0]['d'].forEach(latlng => {
-          this.drawSignalCircle(latlng, 'circle', 'black', 1)
-        })
+      let sampleTemp = this.sample.toString()
+      let clusterTemp = this.cluster.toString()
+      let colorTemp = this.colorValue
+      if (colorTemp === 10) {
+        this.color = d3.schemeCategory10
       } else {
+        this.color = d3.schemeTableau10
       }
+      let currentData = this.dataContainer['cluster'][clusterTemp]['sample'][sampleTemp]
+      this.ODNum = currentData[sampleTemp].length
+      currentData[sampleTemp].forEach((val, i) => {
+        let cn = 'circle' + val[2][1]
+        this.drawSignalCircle(val[1], 'circle ' + cn, this.color[val[2][1]], 1)
+      })
     },
     track () {
-      this.ODNum = this.testRawData[0]['d'].length
       d3.selectAll('.circle').remove()
       d3.selectAll('.road').remove()
-      this.testRawData[0]['d'].forEach(latlng => {
-        this.drawSignalCircle(latlng, 'circle', 'black', 1)
+      let sampleTemp = this.sample.toString()
+      let clusterTemp = this.cluster.toString()
+      let colorTemp = this.colorValue
+      if (colorTemp === 10) {
+        this.color = d3.schemeCategory10
+      } else {
+        this.color = d3.schemeTableau10
+      }
+      let currentData = this.dataContainer['cluster'][clusterTemp]['sample'][sampleTemp]
+      this.ODNum = currentData[sampleTemp].length
+      currentData[sampleTemp].forEach((val, i) => {
+        this.drawSignalLine([val[0], val[1]], this.color[val[2][0]], val[2][0])
       })
     },
     onMapclick (e) {
@@ -381,9 +396,9 @@ export default {
       })
     },
     drawSignalLine (data, c, flag) {
-      L.polyline(data, {color: c, opacity: 0.5, weight: 1, className: 'od' + flag}).addTo(this.myMap)
-      this.drawSignalCircle(data[0], 'od_o' + flag, 'red')
-      this.drawSignalCircle(data[1], 'od_d' + flag, 'black')
+      L.polyline(data, {color: c, opacity: 0.5, weight: 1, className: 'road od' + flag}).addTo(this.myMap)
+      this.drawSignalCircle(data[0], 'circle od_o' + flag, 'red')
+      this.drawSignalCircle(data[1], 'circle od_d' + flag, 'black')
     },
     getData (curData, num) {
       let url = 'http://localhost:8000/first/'
@@ -407,6 +422,15 @@ export default {
       //   })
       //   this.drawLineChart(lineChartData)
       })
+    },
+    async getDataTest () {
+      try {
+        let res = await axios.get('../../static/2017-05-01.json')
+        this.dataContainer = res.data
+      } catch (err) {
+        console.log(err)
+        alert('请求出错！')
+      }
     },
     drawODMatrix (data) {
       let len = Object.keys(data).length
@@ -447,13 +471,23 @@ export default {
         .attr('class', 'colorRect')
     },
     drawLineChart (data) {
+      // let spiltDataX = []
+      // let spiltDataY = []
+      // data.forEach(res => {
+      //   spiltDataX.push(res[0])
+      //   spiltDataY.push(res[1])
+      // })
       let spiltDataX = []
-      let spiltDataY = []
-      data.forEach(res => {
-        spiltDataX.push(res[0])
-        spiltDataY.push(res[1])
-      })
-      let constants = {width: 280, height: 255, top: 20, left: 60, right: 20, bottom: 30}
+      let spiltDataY = data
+      for (let i = 0; i < data.length; i++) {
+        spiltDataX.push(i)
+      }
+      console.log(spiltDataX, spiltDataY)
+      // data.forEach(res => {
+      //   spiltDataX.push(res[0])
+      //   spiltDataY.push(res[1])
+      // })
+      let constants = {width: 280, height: 255, top: 20, left: 65, right: 15, bottom: 30}
       let maxWidth = constants.width - constants.left - constants.right
       let maxHeight = constants.height - constants.top - constants.bottom
       let lineChart = d3.select('.toolbar-middle').append('svg')
@@ -464,11 +498,15 @@ export default {
         .domain([d3.min(spiltDataX), d3.max(spiltDataX)])
         .range([0, maxWidth])
       const yScale = d3.scaleLinear()
-        .domain([d3.min(spiltDataY), d3.max(spiltDataY) + 0.2 * d3.max(spiltDataY)])
+        // eslint-disable-next-line
+        .domain([d3.min(spiltDataY) === 0? d3.min(spiltDataY):d3.min(spiltDataY) - 0.2 * d3.min(spiltDataY), d3.max(spiltDataY) + 0.2 * d3.max(spiltDataY)])
         .range([maxHeight, 0])
+      // let line = d3.line()
+      //   .x(function (d, i) { return xScale(d[0]) })
+      //   .y(function (d, i) { return yScale(d[1]) })
       let line = d3.line()
-        .x(function (d, i) { return xScale(d[0]) })
-        .y(function (d, i) { return yScale(d[1]) })
+        .x(function (d, i) { return xScale(i) })
+        .y(function (d, i) { return yScale(d) })
       const axisLeft = d3.axisLeft(yScale).ticks(5)
       lineChart.append('g')
         .attr('transform', `translate(${constants.left}, 0)`)
@@ -489,8 +527,8 @@ export default {
         .data(data)
         .enter()
         .append('circle')
-        .attr('cx', (d, i) => xScale(d[0]))
-        .attr('cy', (d, i) => yScale(d[1]))
+        .attr('cx', (d, i) => xScale(i))
+        .attr('cy', (d, i) => yScale(d))
         .attr('transform', 'translate(' + constants.left + ', 0)')
         .attr('r', 4)
         .attr('fill', (d, i) => this.color[i])
@@ -528,7 +566,7 @@ export default {
         .attr('class', 'legend')
         .attr('transform', (_, i) => {
           if (i === 0) {
-            return 'rotate(90 20 120)'
+            return 'rotate(90 20 110)'
           }
         })
         .style('font-weight', '700')
